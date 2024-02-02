0d19e3c0b7c8165d018a94be4e016eb0c86069f9
Grace Nieboer <grace.nieboer@outlook.com>
2024-02-01 20:08:37 -0500

    add expandable description for services controlled by state

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,13 +1,37 @@
+import React, { useState } from "react";
+
 const Services = () => {
+    const [open1, setOpen1] = useState(false)
+    const [open2, setOpen2] = useState(false)
+    const [open3, setOpen3] = useState(false)
+
+
     return (
         <div className="flex w-full my-48">
             <div className="w-1/2 mx-10">SERVICES</div>
             <div className="w-1/2 mx-10">
-                <div className="w-full flex justify-between border-b-2 border-t-2 py-5">
-                    <div className="mx-10">Trimming</div>
-                    {/* needs to be a expandable button! use state */}
-                    <div className="mx-10 text-2xl">+</div>
-                </div>
+            
+                   {open1 ? 
+                    <div className="w-full flex justify-between border-b-2 border-t-2 py-5 cursor-pointer">
+                        <div 
+                        className="mx-10 "
+                        onClick={()=>setOpen1(!open1)}>Tree trimming is important for trees of all ages. Done improperly, the tree can be damaged permanently. Tyler has extensive experience in trimming trees to:
+                        <ul>
+                            <li>- prevent decay</li>
+                            <li>- increase lawn health</li>
+                            <li>- improve aesthetic</li>
+                        </ul>
+                        </div>
+                        <div className="mx-10 text-2xl" >+</div>
+                    </div> : 
+                    <div 
+                    className="w-full flex justify-between border-b-2 border-t-2 py-5 cursor-pointer" 
+                    onClick={()=>setOpen1(!open1)}>
+                        <div className="mx-10">Trimming</div>
+                        <div className="mx-10 text-2xl" >+</div>
+                    </div>
+                   } 
+                
             </div>
         </div>
     )
