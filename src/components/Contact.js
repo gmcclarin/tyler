fd24f713f829ad426c7382c7d147cb00a257b446
Grace Nieboer <grace.nieboer@outlook.com>
2024-02-05 21:36:45 -0500

    remove the contact h1 and integrate the yup validation schema

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,47 +25,64 @@ const SignupSchema = Yup.object().shape({
 const Contact = () => {
     return (
         <div>
-     <h1>Contact Us</h1>
-     <Formik
-       initialValues={initialValues}
-       validate={values => {
-         const errors = {};
-         if (!values.email) {
-           errors.email = 'Required';
-         } else if (
-           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-         ) {
-           errors.email = 'Invalid email address';
-         }
-         return errors;
-       }}
-       onSubmit={(values, { setSubmitting }) => {
-         setTimeout(() => {
-           alert(JSON.stringify(values, null, 2));
-           setSubmitting(false);
-         }, 400);
-       }}
-     >
-       {({ isSubmitting }) => (
-         <Form className="max-w-sm mx-auto m-10">
-            <div class="mb-5">
-            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
-                <Field 
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-500 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                type="firstName" 
-                name="firstName" i
-                d="firstName" />
-                <ErrorMessage name="firstName" component="div" />
-           </div>
-           <Field type="password" name="password" />
-           <ErrorMessage name="password" component="div" />
-           <button type="submit" disabled={isSubmitting}>
-             Submit
-           </button>
-         </Form>
-       )}
-     </Formik>
-   </div>
+            <Formik
+            initialValues={initialValues}
+            validationSchema={SignupSchema}
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+                }, 400);
+            }}
+            >
+            {({ isSubmitting }) => (
+                <Form className="max-w-sm mx-auto m-10">
+                    <div class="mb-5">
+                    <label for="firstName" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
+                        <Field 
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
+                        type="firstName" 
+                        name="firstName"
+                        id="firstName" />
+                        <ErrorMessage name="firstName" component="div" />
+                </div>
+                <div class="mb-5">
+                    <label for="LastName" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
+                        <Field 
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                        type="lastName" 
+                        name="lastName"
+                        id="lastName" />
+                        <ErrorMessage name="lastName" component="div" />
+                </div>
+                <div class="mb-5">
+                    <label for="phoneNumber" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone Number</label>
+                        <Field 
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                        type="phoneNumber" 
+                        name="phoneNumber"
+                        id="phoneNumber" />
+                        <ErrorMessage name="phoneNumber" component="div" />
+                </div>
+                <div class="mb-5">
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                        <Field 
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                        type="email" 
+                        name="email"
+                        id="email" />
+                        <ErrorMessage name="email" component="div" />
+                </div>
+                <button 
+                type="submit" 
+                disabled={isSubmitting}
+                className="rounded-xl bg-gray-100 p-2">
+                    Submit
+                </button>
+                </Form>
+            )}
+            </Formik>
+        </div>
     )
 };
 
