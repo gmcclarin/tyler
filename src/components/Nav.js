45c143ee6952b0d531253b5045c2b4c2e658339f
Grace Nieboer <grace.nieboer@outlook.com>
2024-02-13 18:14:01 -0500

    connect the HOME in nav to the actual homepage, add another reveal on the home screen for scrolling effect

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ const Nav = () => {
     return (
         <nav className="fixed top-0 font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-transparent shadow sm:items-baseline w-full">
             <div className="mb-2 sm:mb-0">
-                <a href="#" className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">Home</a>
+                <a href="/" className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">Home</a>
             </div>
             <div>
                 <a href="/services" className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-5">Services</a>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,9 +29,11 @@ const Home = ( ) => {
                     <img className="w-full" src="https://images.unsplash.com/photo-1569227997603-33b9f12af927?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODN8fHRyZWUlMjBidXNpbmVzc3xlbnwwfHwwfHx8MA%3D%3D"/>
                 </div>
                 <div className="w-1/2 m-10 flex justify-center items-centered">
+                    <Reveal>
                     <div className="">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </div>
+                    </Reveal>
                 </div>
             </div>
 
