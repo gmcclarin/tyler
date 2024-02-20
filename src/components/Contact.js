ca565c31197d119bc2e749a9d993f127f54c741a
Grace Nieboer <grace.nieboer@outlook.com>
2024-02-19 20:37:14 -0500

    replace the formik form with input andtext area

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ const initialValues = {
     lastName : "",
     phoneNumber : "",
     fromEmail : "",
-    message : ""
+    message1 : ""
 };
 
 const SignupSchema = Yup.object().shape({
@@ -20,7 +20,7 @@ const SignupSchema = Yup.object().shape({
     phoneNumber : Yup.string(),
     // validate the phone number somehow
     fromEmail: Yup.string().email('Invalid email').required('Required'),
-    message: Yup.string().required("What service would you like?")
+    message1: Yup.string().required("What service would you like?")
   });
 
   const serviceId = process.env.SERVICE_ID;
@@ -66,69 +66,77 @@ const Contact = () => {
                 resetForm({values : initialValues})
             }}
             >
-            {({values, errors, handleChange}) => (
-                <Form className="max-w-sm mx-auto m-10">
+            {({values, errors, handleChange, handleSubmit, touched, handleBlur, isSubmitting}) => (
+                <form onSubmit={handleSubmit}
+                className="max-w-sm mx-auto m-10"
+                >
                     <div className="mb-5">
                     <label htmlFor="firstName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
-                        <Field 
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
-                        type="text" 
-                        name="firstName"
-                        id="firstName"
-                        placeholder="Willow" />
-                        <ErrorMessage name="firstName" component="div" />
+                        <input
+                            type="text"
+                            name="firstName"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.firstName}
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
+                            />
+                {errors.firstName && touched.firstName && errors.firstName}
                 </div>
                 <div className="mb-5">
-                    <label for="LastName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
-                        <Field 
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                        type="lastName" 
-                        name="lastName"
-                        id="lastName"
-                        placeholder="Wood" />
-                        <ErrorMessage name="lastName" component="div" />
+                    <label htmlFor="lastName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
+                        <input
+                            type="lastName"
+                            name="lastName"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.lastName}
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
+                            />
+                {errors.lastName && touched.lastName && errors.lastName}
                 </div>
                 <div className="mb-5">
                     <label htmlFor="phoneNumber" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone Number</label>
-                        <Field 
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                        type="text" 
-                        name="phoneNumber"
-                        id="phoneNumber"
-                        placeholder="555-555-5555" />
-                        <ErrorMessage name="phoneNumber" component="div" />
+                        <input
+                            type="text"
+                            name="phoneNumber"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.phoneNumber}
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
+                            />
+                {errors.phoneNumber && touched.phoneNumber && errors.phoneNumber}
                 </div>
-                <div class="mb-5">
-                    <label htmlFor="fromEmail" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                        <Field 
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                        type="email" 
-                        name="fromEmail"
-                        id="fromEmail"
-                        placeholder="example@gmail.com" />
-                        <ErrorMessage name="fromEmail" component="div" />
+                <div className="mb-5">
+                    <label htmlFor="fromEmail" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                        <input
+                            type="fromEmail"
+                            name="fromEmail"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.fromEmail}
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
+                            />
+                {errors.fromEmail && touched.fromEmail && errors.fromEmail}
                 </div>
-                <div class="mb-5">
-                    <label htmlFor="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Message</label>
-                        <textarea 
-                        type="text"
-                        className="form-textarea mt-1 block w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5" 
-                        rows="5" 
-                        placeholder="Have a question? Want to request a service? Lets talk about it."
-                        id="message"
-                        name="message"
-                        values={values.message}
-                        onChange={handleChange} >
-                        </textarea>
-                        
-                        <ErrorMessage name="message" component="div" />
+
+                <div className="mb-5">
+                    <label htmlFor="message1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Inquiry</label>
+                        <textarea
+                            type="message1"
+                            name="message1"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.message1}
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
+                            />
+                {errors.message1 && touched.message1 && errors.message1}
                 </div>
-                <button 
-                type="submit" 
-                className="rounded-full bg-dark text-white px-3 py-2">
-                    Send
+                <button type="submit" 
+                disabled={isSubmitting}
+                className="bg-white text-black rounded-xl px-2 p-1">
+                  Send
                 </button>
-                </Form>
+              </form>   
             )}
             </Formik>
         </div>
