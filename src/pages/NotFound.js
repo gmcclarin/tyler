dfd74fdcfd4868058a9e75da192d71410049ab74
Grace Nieboer <grace.nieboer@outlook.com>
2024-02-22 18:05:14 -0500

    implement useNavigate hook to bring user back one page to safety

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,15 @@
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+    const navigate = useNavigate();
 
     return(
         <div className="h-screen flex justify-center items-center">
             <div className="text-center">
                 <div className="">OOPS! There's nothing here!</div>
-                <button className=" m-2 px-2 p-1 bg-accent font-bold rounded-full">GO BACK</button>
+                <button
+                onClick={() => navigate(-1)} 
+                className=" m-2 px-2 p-1 bg-accent font-bold rounded-full">GO BACK</button>
             </div>
         </div>
     
