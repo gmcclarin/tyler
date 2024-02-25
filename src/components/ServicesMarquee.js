cde3d14acad2ab0c762f10a2d4101ef04e9be325
Grace Nieboer <grace.nieboer@outlook.com>
2024-02-25 17:41:21 -0500

    create Services Marquee

---
+++ src/components/ServicesMarquee.js
@@ -0,0 +1,16 @@
+
+const ServicesMarquee = () => {
+    return (
+        <div class="overflow-x-hidden">
+            <div class="py-12 animate-marquee whitespace-nowrap ">
+                <span class="mx-4 text-4xl">TRIMMING</span>
+                <span class="mx-4 text-4xl">STUMP GRINDING</span>
+                <span class="mx-4 text-4xl">MULCHING</span>
+                <span class="mx-4 text-4xl">STUMP REMOVAL</span>
+                <span class="mx-4 text-4xl">FULL LANDSCAPE</span>
+            </div>
+        </div>
+    )
+}
+
+export default ServicesMarquee;
--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -1,28 +1,37 @@
 import Nav from "../components/Nav"
 import Footer from "../components/Footer"
+import Reveal from "../components/Reveal"
 
 const ServicesPage = () => {
     return (
         <div>
             <Nav />
-
-            <div className="flex min-w-screen min-h-screen items-center justify-between">
-                <div className="flex w-full justify-center">
-                    <div className="font-bold text-2xl p-10 text-olive hover:text-dark cursor-pointer">TRIMMING</div>
-                    <div className="font-bold text-2xl p-10 text-olive hover:text-dark ">STUMP REMOVAL</div>
-                    <div className="font-bold text-2xl p-10 text-olive hover:text-dark ">MULCHING</div>
-                    <div className="font-bold text-2xl p-10 text-olive hover:text-dark ">THE FULL SITUATION</div>
+           
+            <div className="flex min-w-screen min-h-1/2 items-center justify-between">
+                <div className="flex w-full justify-center my-20">
+                    <div className="font-bold text-2xl p-10 text-olive hover:text-dark cursor-pointer"><a href="#trimmingservice">TRIMMING</a></div>
+                    <div className="font-bold text-2xl p-10 text-olive hover:text-dark  cursor-pointer">STUMP REMOVAL</div>
+                    <div className="font-bold text-2xl p-10 text-olive hover:text-dark  cursor-pointer">MULCHING</div>
+                    <div className="font-bold text-2xl p-10 text-olive hover:text-dark  cursor-pointer">THE FULL SITUATION</div>
                 </div>
             </div>
-
-            <div className="flex min-w-screen min-h-screen items-center justify-center">
-                <div className="flex justify-center w-1/2 p-10">
+            
+            <Reveal>
+            <div id="trimmingservice" className="flex min-w-screen min-h-screen items-center justify-center">
+                <div className="flex justify-center w-2/3 p-10">
                     <img src="https://i.postimg.cc/Sx5ZcK56/IMG-1011.avif" />
                 </div>
-                <div className="w-1/2 flex justify-center text-xl p-20">
-                    Our ISA Certified Arborists and plant healthcare specialists provide tree and landscape assessments, diagnose insect, disease, and structural problems and share solutions to help your trees thrive. Our staff’s experience and state-of-the-art equipment allow us to care for any size property
+                <div className="w-1/3 flex justify-center text-xl p-20 font-bold text-darker">
+                    <div >
+                    <p className="my-5 ">TRIMMING</p>
+                    
+                    Our ISA Certified Arborists and plant healthcare specialists provide tree and landscape assessments, diagnose insect, disease, and structural problems and share solutions to help your trees thrive. Our staff’s experience and state-of-the-art equipment allow us to care for any size property</div>
                 </div>
             </div>
+            </Reveal>
+
+
+
             <Footer />
 
         </div>
