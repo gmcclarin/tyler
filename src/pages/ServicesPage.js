5fdf52fa3f80a45407df168474c8ee46ae628fa3
Grace Nieboer <grace.nieboer@outlook.com>
2024-02-26 13:00:36 -0500

    recreate the services page more simply

--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -8,31 +8,36 @@ const ServicesPage = () => {
         <div>
             <Nav />
            
-            <div className="flex min-w-screen min-h-screen items-center justify-between">
-                {/* <ServicesMarquee /> */}
-                {/* <div className="flex w-full justify-center my-20">
-                    <div className="font-bold text-2xl p-10 text-olive hover:text-dark cursor-pointer"><a href="#trimmingservice">TRIMMING</a></div>
-                    <div className="font-bold text-2xl p-10 text-olive hover:text-dark  cursor-pointer">STUMP REMOVAL</div>
-                    <div className="font-bold text-2xl p-10 text-olive hover:text-dark  cursor-pointer">MULCHING</div>
-                    <div className="font-bold text-2xl p-10 text-olive hover:text-dark  cursor-pointer">THE FULL SITUATION</div>
-                </div> */}
+            <div className="flex min-w-screen min-h-screen items-center justify-between bg-base text-darker">
+
+                <div className="w-1/3 m-5">
+                    <div className="text-4xl mb-10 font-bold">TRIMMING</div>
+                    <div className="text-xl">It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.</div>
+                    <div className="my-10">
+                    <p>__________________________________________</p>
+                    PRICE POINT</div>
+                </div>
+
+                <div className="w-1/3 m-5">
+                    <div className="text-4xl mb-10 font-bold">STUMP REMOVAL</div>
+                    <div className="text-xl">It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.</div>
+                    <div className="my-10">
+                        <p>__________________________________________</p>
+                        SOMETHING ELSE </div>
+                </div>
+
+                <div className="w-1/3 m-5">
+                    <div className="text-4xl mb-10 font-bold">MULCHING</div>
+                    <div className="text-xl">It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.</div>
+                    <div className="my-10">
+                    <p>__________________________________________</p>
+                    ESTIMATE</div>
+                </div>
+
             </div>
+
+            {/* <div><button>READY TO CHAT?</button></div> */}
             
-            <Reveal>
-            <div id="trimmingservice" className="flex min-w-screen min-h-screen items-center justify-center">
-                <div className="flex justify-center w-2/3 p-10">
-                    <img src="https://i.postimg.cc/Sx5ZcK56/IMG-1011.avif" />
-                </div>
-                <div className="w-1/3 flex justify-center text-xl p-20 font-bold text-darker">
-                    <div >
-                    <p className="my-5 ">TRIMMING</p>
-                    
-                    Our ISA Certified Arborists and plant healthcare specialists provide tree and landscape assessments, diagnose insect, disease, and structural problems and share solutions to help your trees thrive. Our staff’s experience and state-of-the-art equipment allow us to care for any size property</div>
-                </div>
-            </div>
-            </Reveal>
-
-
 
             <Footer />
 
