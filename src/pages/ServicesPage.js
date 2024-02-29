bf86692872773be90869a8867f52237b3e9cc061
Grace Nieboer <grace.nieboer@outlook.com>
2024-02-29 17:08:40 -0500

    responsive mobie design done for services page

--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -6,33 +6,33 @@ import MobileNav from "../components/MobileNav";
 
 const ServicesPage = () => {
     return (
-        <div>
+        <div className="overflow-x-hidden">
             <MobileNav />
             <Nav />
            
-            <div className="flex min-w-screen min-h-screen items-center justify-between bg-base text-darker">
+            <div className=" sm:flex min-w-screen min-h-screen items-center justify-between bg-base text-darker">
 
-                <div className="w-1/3 m-5">
+                <div className="w-full sm:w-1/3 p-12 sm:p-5">
                     <div className="text-4xl mb-10 font-bold">TRIMMING</div>
                     <div className="text-xl">It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.</div>
                     <div className="my-10">
-                    <p>__________________________________________</p>
+                    <p>_______________________________</p>
                     PRICE POINT</div>
                 </div>
 
-                <div className="w-1/3 m-5">
+                <div className="w-full sm:w-1/3 p-12 sm:p-5">
                     <div className="text-4xl mb-10 font-bold">STUMP REMOVAL</div>
                     <div className="text-xl">It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.</div>
                     <div className="my-10">
-                        <p>__________________________________________</p>
+                        <p>_______________________________</p>
                         SOMETHING ELSE </div>
                 </div>
 
-                <div className="w-1/3 m-5">
+                <div className="w-full sm:w-1/3 p-12 sm:p-5">
                     <div className="text-4xl mb-10 font-bold">MULCHING</div>
                     <div className="text-xl">It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.</div>
                     <div className="my-10">
-                    <p>__________________________________________</p>
+                    <p>_______________________________</p>
                     ESTIMATE</div>
                 </div>
 
