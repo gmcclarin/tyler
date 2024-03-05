90462f2c0059ab069669366aba8bc40dd847e432
Grace Nieboer <grace.nieboer@outlook.com>
2024-03-04 20:30:10 -0500

    update the services, right before translating to grid instead of flexbox

--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -13,27 +13,27 @@ const ServicesPage = () => {
             <div className=" sm:flex min-w-screen min-h-screen items-center justify-between bg-base text-darker">
 
                 <div className="w-full sm:w-1/3 p-12 sm:p-5">
-                    <div className="text-4xl mb-10 font-bold">TRIMMING</div>
+                    <div className="text-4xl mb-10 font-bold">TREE & BUSH REMOVAL</div>
                     <div className="text-xl">It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.</div>
                     <div className="my-10">
                     <p>_______________________________</p>
-                    PRICE POINT</div>
+                    STARTING AT $199</div>
                 </div>
 
                 <div className="w-full sm:w-1/3 p-12 sm:p-5">
-                    <div className="text-4xl mb-10 font-bold">STUMP REMOVAL</div>
+                    <div className="text-4xl mb-10 font-bold">PRUNING / TRIMMING</div>
                     <div className="text-xl">It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.</div>
                     <div className="my-10">
                         <p>_______________________________</p>
-                        SOMETHING ELSE </div>
+                        STARTING AT $99</div>
                 </div>
 
                 <div className="w-full sm:w-1/3 p-12 sm:p-5">
-                    <div className="text-4xl mb-10 font-bold">MULCHING</div>
+                    <div className="text-4xl mb-10 font-bold">ROOF CLEARANCE</div>
                     <div className="text-xl">It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.</div>
                     <div className="my-10">
                     <p>_______________________________</p>
-                    ESTIMATE</div>
+                    STARTING AT $299</div>
                 </div>
 
             </div>
