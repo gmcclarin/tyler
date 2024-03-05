cb4db15d4a436247d649612c664797f941274ad5
Grace Nieboer <grace.nieboer@outlook.com>
2024-03-04 19:41:37 -0500

    populate Tyler's reviews with his actual google reviews

--- src/components/ReviewCarousel.js
+++ src/components/ReviewCarousel.js
@@ -8,35 +8,65 @@ const ReviewCarousel = () => {
     const reviews = [
         {
             id : 1,
-            r : "tyler was so professional, he really cut that tree down",
-            first :  "Jacquelyn",
-            last : "D"
+            r : "Tyler's Tree Solutions took down and cut up a tall tree close to the house. Tyler did an excellent job. Hard working, timely, reliable and honest!!",
+            first :  "Paul",
+            last : "L"
         },
         {
             id : 2,
-            r : "timber timber timber timber timber and trees falling everywhere wow",
-            first : "Mary",
-            last  : "S"
+            r : "Tyler did a great job trimming three trees and clear brush. Tyler is professional, courteous,  and very knowledgeable. He is neat, cleans up and reasonably priced. Very impressive.  I  highly recommend him!",
+            first : "Andrea",
+            last  : "B"
         },
         {
             id : 3,
-            r : "Tyler removed the stumps in my yard as part of a landscaping project and I loved his work",
-            first : "Robert",
-            last : "Frost"
+            r : "Tyler is very professional and timely in his work. He is knowledgable in removing and trimming trees. Thank you for your devotion to the work you do.",
+            first : "Larry",
+            last : "W"
+        },
+        {
+            id : 4,
+            r : "Tyler did a great job on all five of our maple trees. Also his clean up and hauling away of all the branches was nice to not leave me any work left behind. Thanks, Tyler",
+            first : "Paul",
+            last : "H"
+        },
+        {
+            id : 5,
+            r : "Previous owners let our shrub line grow to a 30’ tall monster which included several trees. The whole line was over a hundred forty feet in length and Tyler’s Tree Solution took care of the whole thing in six flat, as promised. Very pleased!",
+            first : "Brennan",
+            last : "B"
+        },
+        {
+            id : 6,
+            r : "Tyler did a great job trimming three trees, a bush in the yard. Just never look better if you need any kind of yard work or trees, take down, he’s your man.",
+            first : "Tom",
+            last : "A"
+        },
+        {
+            id : 7,
+            r : "Tyler is extremely professional, courteous,  and very knowledgeable. He shows up on time, he is very neat, and reasonably priced. Very impressive.  I would highly recommend him!",
+            first : "Liliana",
+            last : "M"
+        },
+        {
+            id : 8,
+            r : "Great job!  We will definitely have him come again!",
+            first : "Kim",
+            last : "D"
         }
     ]
 
-    const [chosenReview, setChosenReview] = useState(reviews[0])
+    const [chosenReview, setChosenReview] = useState(reviews[5])
     
     
 
-    const handleRandomSelection = () => {
-        const randomElement = reviews[Math.floor(Math.random() * reviews.length)];
-        while (chosenReview == randomElement){
-            randomElement = reviews[Math.floor(Math.random() * reviews.length)]
-        }
-        setChosenReview(randomElement)
-    }
+    // const handleRandomSelection = () => {
+    //     const randomElement = reviews[Math.floor(Math.random() * reviews.length)];
+    //     while (chosenReview == randomElement){
+    //         randomElement = reviews[Math.floor(Math.random() * reviews.length)]
+    //     }
+    //     setChosenReview(randomElement)
+    // }
 
     // these look weird because the id's are not 0 indexed lol
 
