115185325d3b81514b0fa40141ea1d9da4fed161
Grace Nieboer <grace.nieboer@outlook.com>
2024-03-04 20:48:16 -0500

    services page converted to grid for a uniform grid regardless of text length

--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -10,26 +10,50 @@ const ServicesPage = () => {
             <MobileNav />
             <Nav />
            
-            <div className=" sm:flex min-w-screen min-h-screen items-center justify-between bg-base text-darker">
+            <div className="py-24 sm:py-32 p-10 grid grid-cols-3 gap-8  bg-base text-darker">
 
-                <div className="w-full sm:w-1/3 p-12 sm:p-5">
-                    <div className="text-4xl mb-10 font-bold">TREE & BUSH REMOVAL</div>
+                <div className="grid grid-rows-3 items-center">
+                    <div className="text-4xl font-bold">TREE & BUSH REMOVAL</div>
                     <div className="text-xl">It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.</div>
                     <div className="my-10">
                     <p>_______________________________</p>
                     STARTING AT $199</div>
                 </div>
 
-                <div className="w-full sm:w-1/3 p-12 sm:p-5">
-                    <div className="text-4xl mb-10 font-bold">PRUNING / TRIMMING</div>
+                <div className="grid grid-rows-3 items-center">
+                    <div className="text-4xl font-bold">PRUNING / TRIMMING</div>
                     <div className="text-xl">It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.</div>
                     <div className="my-10">
                         <p>_______________________________</p>
                         STARTING AT $99</div>
                 </div>
 
-                <div className="w-full sm:w-1/3 p-12 sm:p-5">
-                    <div className="text-4xl mb-10 font-bold">ROOF CLEARANCE</div>
+                <div className="grid grid-rows-3 items-center">
+                    <div className="text-4xl font-bold">ROOF CLEARANCE</div>
+                    <div className="text-xl">It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.</div>
+                    <div className="my-10">
+                    <p>_______________________________</p>
+                    STARTING AT $299</div>
+                </div>
+
+                <div className="grid grid-rows-3 items-center">
+                    <div className="text-4xl font-bold">CANOPY RAISES</div>
+                    <div className="text-xl">It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.</div>
+                    <div className="my-10">
+                    <p>_______________________________</p>
+                    STARTING AT $299</div>
+                </div>
+
+                <div className="grid grid-rows-3 items-center">
+                    <div className="text-4xl font-bold">DEADWOOD</div>
+                    <div className="text-xl">It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.</div>
+                    <div className="my-10">
+                    <p>_______________________________</p>
+                    STARTING AT $299</div>
+                </div>
+
+                <div className="grid grid-rows-3 items-center">
+                    <div className="text-4xl font-bold">STORM DAMAGE</div>
                     <div className="text-xl">It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.</div>
                     <div className="my-10">
                     <p>_______________________________</p>
