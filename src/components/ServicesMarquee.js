6229c202ce3e770b76c483bd8f3fe103c2b26528
Grace Nieboer <grace.nieboer@outlook.com>
2024-03-04 20:24:01 -0500

    update services marquee

--- src/components/ServicesMarquee.js
+++ src/components/ServicesMarquee.js
@@ -5,51 +5,55 @@ const ServicesMarquee = () => {
             <div>
                 <div className="relative flex overflow-x-hidden">
                     <div className="py-12 animate-marquee whitespace-nowrap ">
+                        <span className="mx-8 text-8xl">REMOVALS</span>
+                        <span className="mx-8 text-8xl">•</span>
+                        <span className="mx-8 text-8xl">PRUNING</span>
+                        <span className="mx-8 text-8xl">•</span>
+                        <span className="mx-8 text-8xl">ROOF CLEARANCE</span>
+                        <span className="mx-8 text-8xl">•</span>
+                        <span className="mx-8 text-8xl">STORM DAMAGE</span>
+                        <span className="mx-8 text-8xl">•</span>
                         <span className="mx-8 text-8xl">TRIMMING</span>
                         <span className="mx-8 text-8xl">•</span>
-                        <span className="mx-8 text-8xl">STUMP GRINDING</span>
-                        <span className="mx-8 text-8xl">•</span>
-                        <span className="mx-8 text-8xl">MULCHING</span>
-                        <span className="mx-8 text-8xl">•</span>
-                        <span className="mx-8 text-8xl">STUMP REMOVAL</span>
-                        <span className="mx-8 text-8xl">•</span>
-                        <span className="mx-8 text-8xl">FULL LANDSCAPE</span>
                     </div>
                     <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
-                    <span className="mx-8 text-8xl">TRIMMING</span>
+                    <span className="mx-8 text-8xl">REMOVALS</span>
                         <span className="mx-8 text-8xl">•</span>
-                        <span className="mx-8 text-8xl">STUMP GRINDING</span>
+                        <span className="mx-8 text-8xl">PRUNING</span>
                         <span className="mx-8 text-8xl">•</span>
-                        <span className="mx-8 text-8xl">MULCHING</span>
+                        <span className="mx-8 text-8xl">ROOF CLEARANCE</span>
                         <span className="mx-8 text-8xl">•</span>
-                        <span className="mx-8 text-8xl">STUMP REMOVAL</span>
+                        <span className="mx-8 text-8xl">STORM DAMAGE</span>
                         <span className="mx-8 text-8xl">•</span>
-                        <span className="mx-8 text-8xl">FULL LANDSCAPE</span>
+                        <span className="mx-8 text-8xl">TRIMMING</span>
+                        <span className="mx-8 text-8xl">•</span>
                     </div>
                 </div>
                 
                 <div className="relative flex overflow-x-hidden">
                     <div className="py-12 animate-marquee3 whitespace-nowrap ">
-                    <span className="mx-8 text-8xl">TRIMMING</span>
+                    <span className="mx-8 text-8xl">REMOVALS</span>
                         <span className="mx-8 text-8xl">•</span>
-                        <span className="mx-8 text-8xl">STUMP GRINDING</span>
+                        <span className="mx-8 text-8xl">PRUNING</span>
                         <span className="mx-8 text-8xl">•</span>
-                        <span className="mx-8 text-8xl">MULCHING</span>
+                        <span className="mx-8 text-8xl">ROOF CLEARANCE</span>
                         <span className="mx-8 text-8xl">•</span>
-                        <span className="mx-8 text-8xl">STUMP REMOVAL</span>
+                        <span className="mx-8 text-8xl">STORM DAMAGE</span>
                         <span className="mx-8 text-8xl">•</span>
-                        <span className="mx-8 text-8xl">FULL LANDSCAPE</span>
+                        <span className="mx-8 text-8xl">TRIMMING</span>
+                        <span className="mx-8 text-8xl">•</span>
                     </div>
                     <div className="absolute top-0 py-12 animate-marquee4 whitespace-nowrap">
-                    <span className="mx-8 text-8xl">TRIMMING</span>
+                    <span className="mx-8 text-8xl">REMOVALS</span>
                         <span className="mx-8 text-8xl">•</span>
-                        <span className="mx-8 text-8xl">STUMP GRINDING</span>
+                        <span className="mx-8 text-8xl">PRUNING</span>
                         <span className="mx-8 text-8xl">•</span>
-                        <span className="mx-8 text-8xl">MULCHING</span>
+                        <span className="mx-8 text-8xl">ROOF CLEARANCE</span>
                         <span className="mx-8 text-8xl">•</span>
-                        <span className="mx-8 text-8xl">STUMP REMOVAL</span>
+                        <span className="mx-8 text-8xl">STORM DAMAGE</span>
                         <span className="mx-8 text-8xl">•</span>
-                        <span className="mx-8 text-8xl">FULL LANDSCAPE</span>
+                        <span className="mx-8 text-8xl">TRIMMING</span>
+                        <span className="mx-8 text-8xl">•</span>
                     </div>
                 </div>
             </div>
