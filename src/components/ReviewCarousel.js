33f60e39600122dcae316eb3c26a2ce1b97321fc
Grace Nieboer <grace.nieboer@outlook.com>
2024-03-04 19:36:20 -0500

    review carousel WORKS

--- src/components/ReviewCarousel.js
+++ src/components/ReviewCarousel.js
@@ -26,20 +26,42 @@ const ReviewCarousel = () => {
         }
     ]
 
-     const [chosenReview, setChosenReview] = useState(reviews[1])
-
+    const [chosenReview, setChosenReview] = useState(reviews[0])
+    
     
 
-     useEffect(() => {
+    const handleRandomSelection = () => {
         const randomElement = reviews[Math.floor(Math.random() * reviews.length)];
-        const interval = setInterval(() => {
-            setChosenReview(randomElement);
-          }, 5000);
-       return () => {
-         clearInterval(interval);
-       };
-     }, []);
+        while (chosenReview == randomElement){
+            randomElement = reviews[Math.floor(Math.random() * reviews.length)]
+        }
+        setChosenReview(randomElement)
+    }
 
+    // these look weird because the id's are not 0 indexed lol
+
+//  reviews = [1, 2, 3, 4, 5, 6]
+   const handleDown = () => {
+     if (chosenReview.id > 1) {
+        setChosenReview(reviews[chosenReview.id - 2])
+     } else {
+        setChosenReview(reviews[reviews.length - 1])
+        
+     }
+     console.log(chosenReview.r)
+   }
+
+   const handleUp = () => {
+    if ( chosenReview.id < reviews.length) {
+        setChosenReview(reviews[chosenReview.id])
+    } else {
+        setChosenReview(reviews[0])
+    }
+    console.log(chosenReview.r)
+   }
+        
+
+   console.log(chosenReview.r)
 
     return (
         <div className="w-screen h-screen flex items-center justify-center bg-darker">
@@ -47,12 +69,14 @@ const ReviewCarousel = () => {
                 <div className="text-white text-4xl -rotate-90 border-b p-6" >CUSTOMER REVIEWS</div>
                 <div className="w-1/2 flex justify-center items-center">
                   
-                        <div className="text-6xl px-16 text-dark">«</div>
+                        <div className="text-6xl mx-16 text-dark cursor-pointer"
+                        onClick={handleDown}>«</div>
                         <div className="">
                             <p className="font-bold text-white text-2xl">{chosenReview.r}</p>
                             <p className="italic text-lg text-white">- {chosenReview.first} {chosenReview.last}</p>
                         </div>
-                        <div className="text-6xl px-16 text-dark">»</div>
+                        <div className="text-6xl mx-16 text-dark cursor-pointer"
+                        onClick={handleUp}>»</div>
                     
                 </div>
    
