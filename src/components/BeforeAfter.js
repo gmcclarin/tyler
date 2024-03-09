c7b8e7cbdf1615345678563ae30b64213d105936
Grace Nieboer <grace.nieboer@outlook.com>
2024-03-08 20:00:54 -0500

    before and after set up correctly

--- src/components/BeforeAfter.js
+++ src/components/BeforeAfter.js
@@ -14,13 +14,13 @@ const BeforeAfter = () => {
         {
             id:2,
             before:"https://i.postimg.cc/7Lm6S1cR/IMG-0049.avif",
-            after:"https://i.postimg.cc/7Lm6S1cR/IMG-0049.avif",
+            after:"https://i.postimg.cc/pVgD9xKF/IMG-0428.avif",
             descr:"ROOF CLEARANCE"
         },
         {
-            id:2,
-            before:"https://i.postimg.cc/7Lm6S1cR/IMG-0049.avif",
-            after:"https://i.postimg.cc/7Lm6S1cR/IMG-0049.avif",
+            id:3,
+            before:"https://i.postimg.cc/4xXHt3ym/IMG-1007.avif",
+            after:"https://i.postimg.cc/nzNQQY9J/IMG-1008.avif",
             descr:"ROOF CLEARANCE"
         }
     ]
@@ -48,13 +48,27 @@ const BeforeAfter = () => {
       }
 
     return (
-        <div className="w-screen h-screen bg-olive relative">
-            <div className="absolute top-10 left-1/4 sm:w-1/3 md:w-1/4 lg:w-1/5"><img src={chosenJob.after} /></div>
-            <div className="absolute bottom-10 right-1/3 sm:w-1/2 md:1/3 lg:w-1/4"><img className="" src={chosenJob.before} /></div>
+        <div className="w-screen h-screen flex justify-between items-center bg-olive relative">
             <div
-            onClick={handleDown}>BACK</div>
+                className="text-9xl mx-5 mb-5 text-right"
+                onClick={handleDown}>«</div>
+            <div className="w-3/4 m-10 flex">
+            
+                <div className="mx-5">
+                    <div className="text-3xl m-1 italic ">BEFORE</div>
+                    <img src={chosenJob.after} />
+                </div>
+                
+                <div className="mx-5">
+                    <img className="" src={chosenJob.before} />
+                    <div className="text-4xl m-3 text-right italic">AFTER</div>
+                </div>
+
+            </div>
+            
             <div
-            onClick={handleUp}>FORWARD</div>
+                className="text-9xl m-5 text-left"
+                onClick={handleUp}>»</div>
         </div>
     )
 };
