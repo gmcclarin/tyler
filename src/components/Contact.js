4e1c77d268758bbf1748db28dc4e071bdf2965e2
Grace Nieboer <grace.nieboer@outlook.com>
2024-03-10 22:11:01 -0400

    readaility increase on contacct form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -37,7 +37,7 @@ const Contact = () => {
     const form = useRef();
 
     return (
-        <div id="contact" className="m-5 sm:m-0">
+        <div id="contact" className="m-5 sm:m-0 text-black">
             <Formik
             initialValues={initialValues}
             validationSchema={SignupSchema}
@@ -72,63 +72,63 @@ const Contact = () => {
                 className="max-w-sm mx-auto m-10"
                 >
                     <div className="mb-5">
-                    <label htmlFor="firstName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
+                    <label htmlFor="firstName" className="block mb-2 text-sm font-medium ">First Name</label>
                         <input
                             type="text"
                             name="firstName"
                             onChange={handleChange}
                             onBlur={handleBlur}
                             value={values.firstName}
-                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
+                            className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
                             />
                 {errors.firstName && touched.firstName && errors.firstName}
                 </div>
                 <div className="mb-5">
-                    <label htmlFor="lastName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
+                    <label htmlFor="lastName" className="block mb-2 text-sm font-medium ">Last Name</label>
                         <input
                             type="lastName"
                             name="lastName"
                             onChange={handleChange}
                             onBlur={handleBlur}
                             value={values.lastName}
-                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
+                            className="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
                             />
                 {errors.lastName && touched.lastName && errors.lastName}
                 </div>
                 <div className="mb-5">
-                    <label htmlFor="phoneNumber" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone Number</label>
+                    <label htmlFor="phoneNumber" className="block mb-2 text-sm font-medium ">Phone Number</label>
                         <input
                             type="text"
                             name="phoneNumber"
                             onChange={handleChange}
                             onBlur={handleBlur}
                             value={values.phoneNumber}
-                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
+                            className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
                             />
                 {errors.phoneNumber && touched.phoneNumber && errors.phoneNumber}
                 </div>
                 <div className="mb-5">
-                    <label htmlFor="fromEmail" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                    <label htmlFor="fromEmail" className="block mb-2 text-sm font-medium ">Email</label>
                         <input
                             type="fromEmail"
                             name="fromEmail"
                             onChange={handleChange}
                             onBlur={handleBlur}
                             value={values.fromEmail}
-                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
+                            className="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
                             />
                 {errors.fromEmail && touched.fromEmail && errors.fromEmail}
                 </div>
 
                 <div className="mb-5">
-                    <label htmlFor="message1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Inquiry</label>
+                    <label htmlFor="message1" className="block mb-2 text-sm font-medium ">Inquiry</label>
                         <textarea
                             type="message1"
                             name="message1"
                             onChange={handleChange}
                             onBlur={handleBlur}
                             value={values.message1}
-                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
+                            className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
                             />
                 {errors.message1 && touched.message1 && errors.message1}
                 </div>
