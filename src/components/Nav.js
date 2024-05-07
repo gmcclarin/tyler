fb5c915cd2c2abfe37e7282cbd6c882568e152c4
Grace Nieboer <grace.nieboer@outlook.com>
2024-05-06 21:40:10 -0400

    add tylers logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,12 @@
+import tree from "../photos/treeLogo.png"
+
 const Nav = () => {
     return (
         <nav className="hidden fixed z-50 top-0 font-sans sm:flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-stone-900 shadow sm:items-baseline w-full">
             <div className="mb-2 sm:mb-0">
-                <a href="/" className="text-2xl font-bold no-underline text-olive hover:text-white">TYLER'S TREE SOLUTIONS</a>
+                <a href="/" className=" flex text-lg md:text-2xl font-bold no-underline text-olive hover:text-white ">TYLER'S TREE SOLUTIONS
+                    <img alt="treeLogo" src={tree} style={{height:"35px"}} />
+                </a>
             </div>
             <div>
                 <a href="/services" className="text-md lg:text-lg font-bold no-underline text-olive hover:text-white ml-5">SERVICES</a>
